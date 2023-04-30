eb7ade47e42802508ef2f3434b5c17d54b74e983
HighestDev <kenneth.danso@amalitech.org>
2023-04-30 22:32:27 +0000

    added a view to users which allows creation or registration

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from .views import *
 
 app_name="users"
 
 urlpatterns = [
-
+  path('register/', RegisterUser.as_view(),name="register_user")
+   
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterUserSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterUser(APIView):
+    permission_class=[AllowAny]
+
+    def post(self,request):
+        serializer=RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_CREATED_201)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
