ac61cd39d7bb6e36c5e2ea34e393243fd2f9fc56
HighestDev <kenneth.danso@amalitech.org>
2023-04-30 14:32:44 +0000

    post models added admin.py

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model=Post
-        fields=('id','title','author','excerpt','content','status')
+        fields=('id','title','slug','author','category','excerpt','content','status')
--- blog_backend/config/base.py
+++ blog_backend/config/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #External packages
     'rest_framework',
+    'corsheaders',
 
     #Local apps
     'blog',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL="users.NewUser"
+
+REST_FRAMEWORK={
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
+
+CORS_ALLOWED_ORIGINS=[
+    "http://localhost:3000"
+]
