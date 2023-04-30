61db8e34c8d0ad31f604375433cddd10c1d6a78d
HighestDev <kenneth.danso@amalitech.org>
2023-04-30 13:58:42 +0000

    blog_api views and serializers created with its right endpoints

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import PostList,PostDetails
 
 app_name='blog_api'
 
 urlpatterns = [
+  path('',PostList.as_view(), name="listcreate"),
+  path('<int:pk>/',PostDetails.as_view(), name="detailcreate")
 
 ]
