505e9b4af528e9a6000662a45ab91a67ac5daaab
HighestDev <kenneth.danso@amalitech.org>
2023-04-30 08:51:11 +0000

    multiple settings environment for project

--- blog_backend/config/base.py
+++ blog_backend/config/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!$zmnk69gto@oxq4*!go_98p-v_t#vb25s116fq!=)0yru$9#b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -73,12 +73,6 @@ WSGI_APPLICATION = 'blog_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from blog_backend.config import base
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_backend.settings')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_backend.config.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_backend.config.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
